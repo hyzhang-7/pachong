537f2b80eee0b69f74f961b283e8b2d71553e81a
hyzhang-7 <69423534+hyzhang-7@users.noreply.github.com>
2022-03-13 03:22:16 +0800

    Delete 第1章 directory

--- 第1章/1_第一个爬虫.py
+++
@@ -1,9 +0,0 @@
-from urllib.request import urlopen
-
-url = 'http://www.baidu.com'
-result = urlopen(url)
-# decode()解码 显示中文
-# print(result.read().decode('utf-8'))
-
-with open('mybaidu.html',mode='w') as f:
-    f.write(result.read().decode('utf-8'))
